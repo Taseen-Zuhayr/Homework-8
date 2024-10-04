681ef624f5b3f73d6be8a131bde621921af409e0
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-10-04 16:19:52 +0600

    homework 8

---
+++ main.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a number : "))
+b = int(input("Enter another number : "))
+c = int(input("Enter another number : "))
+
+temp = a
+a = b
+b = c
+c = temp
+
+print("After swapping:")
+print("a is : ",a)
+print("b is : ",b)
+print("c is : ",c)
